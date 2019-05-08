9232ccfc4199d8cd018ad0b1cc01e06b5833213f
Rodrigo Zea <zearodrigo37@gmail.com>
2019-05-08 16:45:09 -0600

    Commit inicial, agregando funcionalidad que la pistola siga al mouse. Muy basica aun, se desincroniza del mouse.

---
+++ bullet_brawl_proto/Assets/Scripts/GunRotation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunRotation : MonoBehaviour
+{
+    public float speed = 5f;
+
+    // La pistola activamente deberia estar buscando la posicion del mouse o la stick del jugador
+    void Update()
+    {
+        Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, speed * Time.deltaTime);
+        //Vector3 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        //Quaternion rotation = Quaternion.LookRotation(direction);
+        //transform.rotation = Quaternion.Lerp(transform.rotation, rotation, speed * Time.deltaTime);
+        //Debug.Log(transform.rotation);
+    }
+}
